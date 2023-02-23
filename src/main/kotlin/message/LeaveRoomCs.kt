192748376c052612ee7bf7d77b309b9da9ff440f
Death <546115696@qq.com>
2023-02-23 15:29:11 +0800

    修复退出房间name丢失问题

--- src/main/kotlin/message/LeaveRoomCs.kt
+++ src/main/kotlin/message/LeaveRoomCs.kt
@@ -39,7 +39,7 @@ class LeaveRoomCs : Handler {
             Store.removeRoom(room.roomId)
         else
             Store.putRoom(room)
-        Store.putPlayer(Player(token))
+        Store.putPlayer(Player(token = token,roomId = "",name = player.name))
         Store.notifyPlayerInfo(token, protoName) // 已经退出了，所以这里只能通知到自己
         // 需要再通知房间里的其他人
         var message: Message? = null
