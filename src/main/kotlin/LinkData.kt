b90669302dde2d911579bd4df4f01f21a688e630
CuteReimu <415551921@qq.com>
2023-04-13 19:04:21 +0800

    link赛支持无导播模式

--- src/main/kotlin/LinkData.kt
+++ src/main/kotlin/LinkData.kt
@@ -1,36 +1,13 @@
 package org.tfcc.bingo
 
-data class LinkData(
-    var linkIdxA: ArrayList<Int>,
-    var linkIdxB: ArrayList<Int>,
-    var startMsA: Long,
-    var endMsA: Long,
-    var startMsB: Long,
-    var endMsB: Long
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+class LinkData {
+    var linkIdxA = ArrayList<Int>()
+    var linkIdxB = ArrayList<Int>()
+    var startMsA = 0L
+    var endMsA = 0L
+    var startMsB = 0L
+    var endMsB = 0L
 
-        other as LinkData
-
-        if (linkIdxA != other.linkIdxA) return false
-        if (linkIdxB != other.linkIdxB) return false
-        if (startMsA != other.startMsA) return false
-        if (endMsA != other.endMsA) return false
-        if (startMsB != other.startMsB) return false
-        if (endMsB != other.endMsB) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = linkIdxA.hashCode()
-        result = 31 * result + linkIdxB.hashCode()
-        result = 31 * result + startMsA.hashCode()
-        result = 31 * result + endMsA.hashCode()
-        result = 31 * result + startMsB.hashCode()
-        result = 31 * result + endMsB.hashCode()
-        return result
-    }
+    fun selectCompleteA() = (startMsA > 0 || startMsB > 0) && linkIdxA.last() == 24
+    fun selectCompleteB() = (startMsA > 0 || startMsB > 0) && linkIdxB.last() == 20
 }
--- src/main/kotlin/RoomTypeLink.kt
+++ src/main/kotlin/RoomTypeLink.kt
@@ -9,7 +9,7 @@ object RoomTypeLink : RoomType {
     override fun onStart(room: Room) {
         room.spellStatus!![0] = SpellStatus.LEFT_SELECT
         room.spellStatus!![4] = SpellStatus.RIGHT_SELECT
-        val linkData = LinkData(ArrayList(), ArrayList(), 0, 0, 0, 0)
+        val linkData = LinkData()
         linkData.linkIdxA.add(0)
         linkData.linkIdxB.add(4)
         room.linkData = linkData
@@ -28,73 +28,91 @@ object RoomTypeLink : RoomType {
 //        SpellLog.logSpellOperate(status, room.spells!![idx], token)
         return when (token) {
             room.host -> status
+
             room.players[0] -> {
-                if ((room.linkData!!.startMsA > 0 || room.linkData!!.startMsB > 0) &&
-                    room.linkData!!.linkIdxA.last() == 24
-                ) throw HandlerException("选卡已结束")
-                when (status) {
-                    SpellStatus.LEFT_SELECT -> {
-                        if (room.linkData!!.linkIdxA.contains(idx))
-                            throw HandlerException("已经选了这张卡")
-                        val idx0 = room.linkData!!.linkIdxA.last()
-                        val diff = abs(idx - idx0)
-                        if (idx0 == 24 || diff != 1 && diff != 4 && diff != 5 && diff != 6)
-                            throw HandlerException("不合理的选卡")
-                        room.linkData!!.linkIdxA.add(idx)
-                        if (st == SpellStatus.RIGHT_SELECT) SpellStatus.BOTH_SELECT else status
-                    }
+                if (!room.linkData!!.selectCompleteA()) { // 选卡阶段
+                    when (status) {
+                        SpellStatus.LEFT_SELECT -> {
+                            if (room.linkData!!.linkIdxA.contains(idx))
+                                throw HandlerException("已经选了这张卡")
+                            val idx0 = room.linkData!!.linkIdxA.last()
+                            if (idx0 == 24 || !(idx near idx0))
+                                throw HandlerException("不合理的选卡")
+                            room.linkData!!.linkIdxA.add(idx)
+                            if (st == SpellStatus.RIGHT_SELECT) SpellStatus.BOTH_SELECT else status
+                        }
 
-                    SpellStatus.NONE -> {
-                        if (room.linkData!!.linkIdxA.size <= 1)
-                            throw HandlerException("初始选卡不能删除")
-                        if (room.linkData!!.linkIdxA.last() != idx)
-                            throw HandlerException("只能删除最后一张卡")
-                        room.linkData!!.linkIdxA.removeLast()
-                        if (st == SpellStatus.BOTH_SELECT) SpellStatus.RIGHT_SELECT else status
-                    }
+                        SpellStatus.NONE -> {
+                            if (room.linkData!!.linkIdxA.size <= 1)
+                                throw HandlerException("初始选卡不能删除")
+                            if (room.linkData!!.linkIdxA.last() != idx)
+                                throw HandlerException("只能删除最后一张卡")
+                            room.linkData!!.linkIdxA.removeLast()
+                            if (st == SpellStatus.BOTH_SELECT) SpellStatus.RIGHT_SELECT else status
+                        }
 
-                    else -> {
-                        if (room.host.isNotEmpty())
-                            throw HandlerException("权限不足")
-                        status
+                        else -> throw HandlerException("权限不足")
+                    }
+                } else { // 收卡阶段
+                    if (room.host.isNotEmpty())
+                        throw HandlerException("权限不足")
+                    when (status) {
+                        SpellStatus.LEFT_GET ->
+                            if (st == SpellStatus.RIGHT_GET) SpellStatus.BOTH_GET else status
+
+                        SpellStatus.NONE ->
+                            if (st == SpellStatus.BOTH_GET) SpellStatus.RIGHT_GET else status
+
+                        else -> throw HandlerException("权限不足")
                     }
                 }
             }
 
             room.players[1] -> {
-                if ((room.linkData!!.startMsA > 0 || room.linkData!!.startMsB > 0) &&
-                    room.linkData!!.linkIdxB.last() == 20
-                ) throw HandlerException("选卡已结束")
-                when (status) {
-                    SpellStatus.RIGHT_SELECT -> {
-                        if (room.linkData!!.linkIdxB.contains(idx))
-                            throw HandlerException("已经选了这张卡")
-                        val idx0 = room.linkData!!.linkIdxB.last()
-                        val diff = abs(idx - idx0)
-                        if (idx0 == 20 || diff != 1 && diff != 4 && diff != 5 && diff != 6)
-                            throw HandlerException("不合理的选卡")
-                        room.linkData!!.linkIdxB.add(idx)
-                        if (st == SpellStatus.LEFT_SELECT) SpellStatus.BOTH_SELECT else status
-                    }
+                if (!room.linkData!!.selectCompleteB()) { // 选卡阶段
+                    when (status) {
+                        SpellStatus.RIGHT_SELECT -> {
+                            if (room.linkData!!.linkIdxB.contains(idx))
+                                throw HandlerException("已经选了这张卡")
+                            val idx0 = room.linkData!!.linkIdxB.last()
+                            if (idx0 == 20 || !(idx near idx0))
+                                throw HandlerException("不合理的选卡")
+                            room.linkData!!.linkIdxB.add(idx)
+                            if (st == SpellStatus.LEFT_SELECT) SpellStatus.BOTH_SELECT else status
+                        }
 
-                    SpellStatus.NONE -> {
-                        if (room.linkData!!.linkIdxB.size <= 1)
-                            throw HandlerException("初始选卡不能删除")
-                        if (room.linkData!!.linkIdxB.last() != idx)
-                            throw HandlerException("只能删除最后一张卡")
-                        room.linkData!!.linkIdxB.removeLast()
-                        if (st == SpellStatus.BOTH_SELECT) SpellStatus.LEFT_SELECT else status
-                    }
+                        SpellStatus.NONE -> {
+                            if (room.linkData!!.linkIdxB.size <= 1)
+                                throw HandlerException("初始选卡不能删除")
+                            if (room.linkData!!.linkIdxB.last() != idx)
+                                throw HandlerException("只能删除最后一张卡")
+                            room.linkData!!.linkIdxB.removeLast()
+                            if (st == SpellStatus.BOTH_SELECT) SpellStatus.LEFT_SELECT else status
+                        }
 
-                    else -> {
-                        if (room.host.isNotEmpty())
-                            throw HandlerException("权限不足")
-                        status
+                        else -> throw HandlerException("权限不足")
+                    }
+                } else { // 收卡阶段
+                    if (room.host.isNotEmpty())
+                        throw HandlerException("权限不足")
+                    when (status) {
+                        SpellStatus.RIGHT_GET ->
+                            if (st == SpellStatus.LEFT_GET) SpellStatus.BOTH_GET else status
+
+                        SpellStatus.NONE ->
+                            if (st == SpellStatus.BOTH_GET) SpellStatus.LEFT_GET else status
+
+                        else -> throw HandlerException("权限不足")
                     }
                 }
             }
 
             else -> throw HandlerException("内部错误")
         }
+    }
+
+    private infix fun Int.near(other: Int): Boolean {
+        val diff = abs(this - other)
+        return diff == 1 || diff == 4 || diff == 5 || diff == 6
     }
 }
