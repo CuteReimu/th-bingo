f88cc4c3f09e7421038a0960961fde1d1d9683dd
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-04-15 00:55:02 +0800

    修复无房主非人机模式不能选卡的bug

--- src/main/kotlin/message/UpdateSpellStatusHandler.kt
+++ src/main/kotlin/message/UpdateSpellStatusHandler.kt
@@ -17,30 +17,8 @@ object UpdateSpellStatusHandler : RequestHandler {
         room.started || throw HandlerException("游戏还没开始")
         if (room.host != null) { // 自己是房主则有权限
             room.host === player || throw HandlerException("没有权限")
-        } else { // 无房主模式，自己是选手并且另一个选手是机器人才有权限
-            val oldStatus = room.spellStatus!![idx]
-            player in room.players && room.players.any { it!!.name == Store.ROBOT_NAME } || run {
-                // 或者标准赛自己可以改自己的状态
-                room.type is RoomTypeNormal || return@run false
-                val index = room.players.indexOf(player)
-                when (index) {
-                    0 -> oldStatus == LEFT_SELECT && spellStatus == NONE || // 左选 -> 空
-                        oldStatus == NONE && spellStatus == LEFT_SELECT || // 空 -> 左选
-                        oldStatus == BOTH_SELECT && spellStatus == RIGHT_SELECT || // 双选 -> 右选
-                        oldStatus == RIGHT_SELECT && spellStatus == BOTH_SELECT || // 右选 -> 双选
-                        spellStatus == LEFT_GET || // 任意 -> 左收
-                        oldStatus == LEFT_GET && spellStatus in listOf(NONE, LEFT_SELECT) // 左收 -> 左选|空
-
-                    1 -> oldStatus == RIGHT_SELECT && spellStatus == NONE || // 右选 -> 空
-                        oldStatus == NONE && spellStatus == RIGHT_SELECT || // 空 -> 右选
-                        oldStatus == BOTH_SELECT && spellStatus == LEFT_SELECT || // 双选 -> 左选
-                        oldStatus == LEFT_SELECT && spellStatus == BOTH_SELECT || // 左选 -> 双选
-                        spellStatus == RIGHT_GET || // 任意 -> 右收
-                        oldStatus == RIGHT_GET && spellStatus in listOf(NONE, RIGHT_SELECT) // 右收 -> 右选|空
-
-                    else -> false
-                }
-            } || throw HandlerException("没有权限")
+        } else { // 无房主模式，只要是选手就有权限
+            player in room.players
         }
         if (room.host === player && room.linkData != null) {
             if (!room.linkData!!.selectCompleteA() || room.linkData!!.selectCompleteB()) {
